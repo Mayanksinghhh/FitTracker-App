64b1c8f3b12d3c39fae5de4c75a4dd6a33a079ad
Mayanksinghhh <128398164+Mayanksinghhh@users.noreply.github.com>
2025-04-04 13:43:46 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,16 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'FitTrack',
   description: 'FitTrack is a web application that helps you track your workouts and fitness goals.',
+  icons: {
+    icon: '/images.png',
+    shortcut: '/images.png',
+    apple: '/images.png',
+    other: {
+      rel: 'apple-touch-icon-precomposed',
+      url: '/apple-touch-icon-precomposed.png',
+    },
+  },
 }
-
 export default function RootLayout({
   children,
 }: Readonly<{
